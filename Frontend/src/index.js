1a59ba02bca4cd247828cbf9bb3d3f0cb5b02e1c
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-01 23:07:27 +0700

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Sidebar from './Sidebar';
+import LoginPage from './LoginPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Sidebar />
+    <LoginPage />
   </React.StrictMode>
 );
 
