edd633511efeb5027d30829eae8c338e74488087
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 18:42:39 +0700

    Update Notification.js

--- Frontend/src/pages/Notification.js
+++ Frontend/src/pages/Notification.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Typography, TextField, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import { Box, Button, Chip } from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesome
-import { faCog } from '@fortawesome/free-solid-svg-icons'; // Import Settings Icon
+import { Typography, } from '@mui/material';
+
 
 
 
