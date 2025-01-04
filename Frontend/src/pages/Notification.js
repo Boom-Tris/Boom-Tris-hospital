da3a0253f05a3b12f7ef4d57b1817f617ed17aa0
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 18:41:57 +0700

    Update Notification.js

--- Frontend/src/pages/Notification.js
+++ Frontend/src/pages/Notification.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, TextField, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { Box, Button, Chip } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesome
+import { faCog } from '@fortawesome/free-solid-svg-icons'; // Import Settings Icon
+
+
+
 
 const Notification = () => {
-  
   return (
     <div>
-      <Typography variant="h4" gutterBottom> Notification </Typography>
-      <Typography variant="body1">การแจ้งเตือน </Typography>
+      <Typography variant="h4" gutterBottom>
+        Notification
+      </Typography>
+      <Typography variant="body1">การแจ้งเตือน</Typography>
+
+     
     </div>
-  )
-  
+  );
 };
 
 export default Notification;
