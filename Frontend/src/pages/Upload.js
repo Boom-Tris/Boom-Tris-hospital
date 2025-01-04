4824105257643db64b1f22943e403dfbd8871b38
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 13:40:56 +0700

    Update Upload.js

--- Frontend/src/pages/Upload.js
+++ Frontend/src/pages/Upload.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 const Upload = () => {
-  return <Typography variant="body1">พื้นที่สำหรับอัพโหลดไฟล์</Typography>;
+
+  return (
+    <div>
+       <Typography variant="h4" gutterBottom> Upload </Typography>
+      <Typography variant="body1">พื้นที่สำหรับอัพโหลดไฟล์</Typography>
+    </div>
+  )
+  
 };
 
 export default Upload;
