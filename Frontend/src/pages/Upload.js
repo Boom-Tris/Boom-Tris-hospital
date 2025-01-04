86dd54fa79f903d108889f4fa59c4d6283a0362e
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 13:15:10 +0700

    Create Upload.js

---
+++ Frontend/src/pages/Upload.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Upload = () => {
+  return <Typography variant="body1">พื้นที่สำหรับอัพโหลดไฟล์</Typography>;
+};
+
+export default Upload;
