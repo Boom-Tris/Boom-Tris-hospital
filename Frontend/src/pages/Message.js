3c9da4bdab0da0b9244e43447849a7503b780b26
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 13:15:03 +0700

    Create Message.js

---
+++ Frontend/src/pages/Message.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Message = () => {
+  return <Typography variant="body1">ข้อความของคุณ</Typography>;
+};
+
+export default Message;
