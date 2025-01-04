2ad33efacc55da4e7d292ee48f5c1d5720703ebe
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 13:40:50 +0700

    Update Message.js

--- Frontend/src/pages/Message.js
+++ Frontend/src/pages/Message.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 const Message = () => {
-  return <Typography variant="body1">ข้อความของคุณ</Typography>;
+  
+  
+
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom> Message </Typography>
+      <Typography variant="body1">ข้อความของคุณ </Typography>
+    </div>
+  )
+  
 };
 
 export default Message;
