9b61afd0d0c3b1405c42b33384a0a745088229c3
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 13:15:08 +0700

    Create Notification.js

---
+++ Frontend/src/pages/Notification.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Notification = () => {
+  return <Typography variant="body1">การแจ้งเตือน</Typography>;
+};
+
+export default Notification;
