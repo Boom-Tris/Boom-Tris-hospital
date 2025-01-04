f43ebc955aa3cb6c08d6fdc0f656f6d698e81058
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 13:15:06 +0700

    Create Profile.js

---
+++ Frontend/src/pages/Profile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const Profile = () => {
+  return <Typography variant="body1">โปรไฟล์ของคุณ</Typography>;
+};
+
+export default Profile;
