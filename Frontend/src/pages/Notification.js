a687e899f1c79f3c1da1cbbee5072300f9594a2b
Boom-Tris <boomtris.jontama@gmail.com>
2025-01-04 13:40:52 +0700

    Update Notification.js

--- Frontend/src/pages/Notification.js
+++ Frontend/src/pages/Notification.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Typography } from '@mui/material';
 
 const Notification = () => {
-  return <Typography variant="body1">การแจ้งเตือน</Typography>;
+  
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom> Notification </Typography>
+      <Typography variant="body1">การแจ้งเตือน </Typography>
+    </div>
+  )
+  
 };
 
 export default Notification;
