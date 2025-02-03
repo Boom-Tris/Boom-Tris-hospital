e08d4f2ba206f6a3fa508a882d9964fd60cdb9ab
Boom-Tris <boomtris.jontama@gmail.com>
2025-02-03 17:41:25 +0700

    33

--- Frontend/src/Sidebar.js
+++ Frontend/src/Sidebar.js
@@ -46,7 +46,7 @@ const Sidebar = ({ children }) => {
         <List>
           {menuItems.map((item) => (
             <ListItem
-              button
+              button="true" // Convert to string
               key={item.text}
               className={`menu-item ${selectedMenu === item.text ? 'selected' : ''}`}
               onClick={() => handleMenuClick(item.text)}
